f0e546267a4c9cb40d55a8ecac3cb38c50a31a1b
TaraLoveCats <280895202@qq.com>
2020-03-18 19:10:09 +0800

    add analyzer

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 const withCss = require('@zeit/next-css') //使next可以加载css
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+})
+const webpack = require('webpack')
 
-module.exports = withCss()
+module.exports = withBundleAnalyzer(withCss({
+    webpack: (config) => {
+        config.plugins.push(new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/))
+        return config
+    }
+}))
